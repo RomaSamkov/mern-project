51f71cfbd120135f967755cbec647f2f1cce8431
RomaSamkov <Romansamkov.rs@gmail.com>
2022-10-27 23:06:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 dotenv.config();
+import { validationResult } from "express-validator";
+import { registerValidation } from "./validations/auth.js";
+import UserModel from "./models/User.js";
 
 const { DB_HOST } = process.env;
 
@@ -14,23 +18,26 @@ mongoose
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.post("/auth/register", registerValidation, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
 
-app.post("/auth/login", (req, res) => {
-  console.log(req.body);
-  const token = jwt.sign(
-    {
-      email: req.body.email,
-      fullName: "User",
-    },
-    "secret"
-  );
-  res.json({
-    message: "success",
-    token,
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const doc = UserModel({
+    email: req.body.email,
+    passwordHash,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
   });
+
+  const user = await doc.save();
+
+  res.json(user);
 });
 
 app.listen(4444, (error) => {
