84247caa0af4904a4b129d01c74b53d09225528a
RomaSamkov <Romansamkov.rs@gmail.com>
2022-10-28 22:45:11 +0300

    Create UserController.js

---
+++ controllers/UserController.js
@@ -0,0 +1,92 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { validationResult } from "express-validator";
+import UserModel from "../models/User.js";
+
+export const register = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = UserModel({
+      email: req.body.email,
+      passwordHash: hash,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+    });
+
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret",
+      { expiresIn: "1h" }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "No register" });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const isValidPass = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    );
+    if (!isValidPass) {
+      return res.status(400).json({ message: "Not correct login or password" });
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret",
+      { expiresIn: "1h" }
+    );
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Not Authorized" });
+  }
+};
+
+export const getCurrent = async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({ userData });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "No access" });
+  }
+};
