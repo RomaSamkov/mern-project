4c5dbda279431e764f09731f37be9b7eecb60c7f
RomaSamkov <Romansamkov.rs@gmail.com>
2022-10-28 22:45:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 import express from "express";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 dotenv.config();
-import { validationResult } from "express-validator";
-import { registerValidation } from "./validations/auth.js";
-import UserModel from "./models/User.js";
+import { registerValidation, loginValidation } from "./validation.js";
+import checkAuth from "./utils/checkAuth.js";
+import * as UserController from "./controllers/UserController.js";
 
 const { DB_HOST } = process.env;
 
@@ -18,27 +16,11 @@ mongoose
 const app = express();
 app.use(express.json());
 
-app.post("/auth/register", registerValidation, async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
-  }
+app.post("/auth/login", loginValidation, UserController.login);
 
-  const password = req.body.password;
-  const salt = await bcrypt.genSalt(10);
-  const passwordHash = await bcrypt.hash(password, salt);
+app.post("/auth/register", registerValidation, UserController.register);
 
-  const doc = UserModel({
-    email: req.body.email,
-    passwordHash,
-    fullName: req.body.fullName,
-    avatarUrl: req.body.avatarUrl,
-  });
-
-  const user = await doc.save();
-
-  res.json(user);
-});
+app.get("/auth/current", checkAuth, UserController.getCurrent);
 
 app.listen(4444, (error) => {
   if (error) {
